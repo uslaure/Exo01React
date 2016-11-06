3054d81f247c473abc4c30737f3114a428e40ad7
uslaure <laure-ashley.graimont@supinternet.fr>
2016-11-06 16:36:05 +0100

    Add tchat interface

--- index.android.js
+++ index.android.js
@@ -1,22 +1,36 @@
-import { createStore } from 'redux'
+import React, { Component } from 'react';
+import { AppRegistry,
+ Navigator,
+ Text,
+  View,
+  TextInput,
+} from 'react-native';
 
-function todos(state = [], action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return state.concat([ action.text ])
-    default:
-      return state
-  }
+class Main extends Component{
+	constructor(props) {
+		super(props);
+		this.state = { message: '' };
+		this.messages = [];
+	}
+
+	render(){
+		return (
+			<View style = {{ flex: 1 }}>
+				<View style = {{ backgroundColor: '#006a56', padding: 20 }}>
+					<Text style = {{ color: 'white'}}>Tchat Exercice</Text>
+				</View>
+				<View style = {{ backgroundColor: '#fff', flex: 1 }}>
+					<Text style = {{ fontWeight: 'bold', fontSize: 14 }}></Text>
+				</View>
+				<View style = {{ flex: 0.1 }}>
+					<TextInput
+						style = {{ borderWidth: 1, backgroundColor: '#006a56', color:'#fff', fontSize:16 }}
+						onChangeText = { (message) => this.setState({ message }) }
+        				value = { this.state.message }
+					/>
+				</View>
+			</View>
+		)
+	}
 }
-
-let store = createStore(todos, [ 'Use Redux' ])
-
-store.dispatch({
-  type: 'ADD_TODO',
-  text: 'Read the docs'
-})
-
-console.log(store.getState())
-// [ 'Use Redux', 'Read the docs' ]
-
-AppRegistry.registerComponent('Day01', () => Day01);
+AppRegistry.registerComponent('Day01', () => Main);
